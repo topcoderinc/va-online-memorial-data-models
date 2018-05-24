090500f141223312e25afadb5a59766576f12dd0
Dustin W <dweaver@appirio.com>
2018-05-24 15:04:41 -0500

    removed SSL

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ const constants = require('./constants');
 // initialize database connection
 const sequelize = new Sequelize(config.dbConfig.db_url, {
   logging: null,
-  ssl: true,
   // native: 'true',
   dialect: 'postgres'
 });
