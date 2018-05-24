d5a8dd8ab299c39bd9728935aa299b849ef4e230
Dustin W <dweaver@appirio.com>
2018-05-24 14:34:32 -0500

    added SSL

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const constants = require('./constants');
 // initialize database connection
 const sequelize = new Sequelize(config.dbConfig.db_url, {
   logging: null,
+  ssl: true,
   // native: 'true',
   dialect: 'postgres'
 });
